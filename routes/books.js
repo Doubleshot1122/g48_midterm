a832edcf0573f054f583abc6232da360e31bf335
Doubleshot1122 <skibo1122@yahoo.com>
2017-05-08 15:18:56 -0700

    Edit books -page created -data showing up on page -update not working and neither is validation

--- routes/books.js
+++ routes/books.js
@@ -2,37 +2,22 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db/connections.js');
 
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   db('books')
-  .select('books.*', 'authors.first_name', 'authors.last_name', 'authors.id as aid')
-  .innerJoin('book_author', 'books.id', 'book_author.book_id')
-  .innerJoin('authors', 'book_author.author_id', 'authors.id')
-
-  .then(results => {
-    let reducedResults = []
-    let authors = []
-    results.forEach(el => {
-      console.log("=============");
-      console.log("el", el);
-      console.log("=============");
-      reducedResults.push({
-        id: el.id,
-        title: el.title,
-        genre: el.genre,
-        description: el.description,
-        portrait_url: el.portrait_url
-      })
-      // if (1 === 1) {
-      //   reducedResults[]
-      // }
-    })
-    // console.log(results);
-    // console.log(results.length);
-    return results;
-  })
   .then(results => {
     res.render('books/index', { title: 'Galvanize Reads', results});
   })
 });
+
+router.get('/:index/edit', (req, res, next) => {
+  db('books').where('id', req.params.index).first()
+  .then(result => {
+      res.render('books/edit', { title: 'Galvanize Reads', subTitle: 'Edit book', result});
+  })
+});
+
+router.get('/new', (req, res, next) => {
+  res.render('books/new', { title: 'Galvanize Reads', subTitle: 'Add a new book'});
+})
 
 module.exports = router;
