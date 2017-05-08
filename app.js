9ddd99168d39bdd0d5c0fbf56b49a237107f5eb2
Doubleshot1122 <skibo1122@yahoo.com>
2017-05-08 10:44:31 -0700

    added required items to app.js and created the home page minus header info

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var engine = require('ejs-locals');
+var methodOverride = require('method-override');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -11,12 +13,14 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
+app.engine('ejs', engine)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(methodOverride('_method'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
